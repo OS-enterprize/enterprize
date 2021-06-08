04bdb0c9a6ee920ac31de53c836ef1c7b173168e
James Espy <jamesespy@Jamess-MacBook-Air.local>
2021-06-08 15:18:56 -0500

    separated export statements in actions.js

--- src/client/actions/actions.js
+++ src/client/actions/actions.js
@@ -1,39 +1,37 @@
 import * as types from './actionTypes.js';
 import { dispatch } from 'react-redux';
 
-export default {
-  const login = (loggedIn) => ({
-    type: types.LOG_IN,
-    payload: loggedIn
-  })
+export const login = (loggedIn) => ({
+  type: types.LOG_IN,
+  payload: loggedIn
+});
 
-  const logout = (loggedOut) => ({
-    type: types.LOG_OUT,
-    payload: loggedOut
-  })
+export const logout = (loggedOut) => ({
+  type: types.LOG_OUT,
+  payload: loggedOut
+});
 
-  const createUser = (newUser) => ({
-    type: types.CREATE_USER,
-    payload: newUser
-  })
+export const createUser = (newUser) => ({
+  type: types.CREATE_USER,
+  payload: newUser
+});
 
-  const getProgress = (progress) => ({
-    type: types.GET_PROGRESS,
-    payload: progress
-  })
+export const getProgress = (progress) => ({
+  type: types.GET_PROGRESS,
+  payload: progress
+});
 
-  const addProgress = (progress) => ({
-    type: types.ADD_PROGRESS,
-    payload: progress
-  })
+export const addProgress = (progress) => ({
+  type: types.ADD_PROGRESS,
+  payload: progress
+});
 
-  const deleteProgress = (progress) => ({
-    type: types.DELETE_PROGRESS,
-    payload: progress
-  })
+export const deleteProgress = (progress) => ({
+  type: types.DELETE_PROGRESS,
+  payload: progress
+});
 
-  const updateProgress = (progress) => ({
-    type: types.UPDATE_PROGRESS,
-    payload: progress
-  })
-};
+export const updateProgress = (progress) => ({
+  type: types.UPDATE_PROGRESS,
+  payload: progress
+});
