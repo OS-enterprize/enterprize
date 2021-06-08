f3112283fcdb3a15eab543c614cc15817dd4c8b4
James Espy <jamesespy@Jamess-MacBook-Air.local>
2021-06-08 13:51:44 -0500

    added actionTypes

---
+++ src/client/actions/actionTypes.js
@@ -0,0 +1,10 @@
+export const Log
+
+
+export const LOG_IN = "LOG_IN";
+export const LOG_OUT = "LOG_OUT";
+export const CREATE_USER = "CREATE-USER";
+export const GET_PROGRESS = "GET_PROGRESS";
+export const ADD_PROGRESS = "ADD_PROGRESS";
+export const DELETE_PROGRESS = "DELETE_PROGRESS";
+export const UPDATE_PROGRESS = "UPDATE_PROGRESS";
