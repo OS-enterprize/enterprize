a5e33a0dda79bbc6acb30091af7ff86b088f56e6
Wesley Jia <wesley.jia94@gmail.com>
2021-06-09 11:19:18 -0400

    Revised tests to work with connected Redux store

--- __tests__/client/components/app-main.test.js
+++ __tests__/client/components/app-main.test.js
@@ -9,10 +9,13 @@ Tested containers include:
 
 import enzyme from 'enzyme'
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 // const ShallowRenderer = require('react-test-renderer/shallow')
 
 import MainPage from '../../../src/client/pages/MainPage.jsx';
-import HeaderContainer from '../../../src/client/containers/HeaderContainer.jsx';
+import ConnectedHeaderContainer, { HeaderContainer } from '../../../src/client/containers/HeaderContainer.jsx';
+import HeaderNav from '../../../src/client/components/HeaderNav.jsx';
 import PipelineContainer from '../../../src/client/containers/PipelineContainer.jsx';
 import BoardsContainer from '../../../src/client/containers/BoardsContainer.jsx';
 
@@ -22,12 +25,12 @@ describe('top-level components', () => {
 
     const wrapper = enzyme.shallow(<MainPage />);
 
-    it('should only render three child mnodes', () => {
+    it('should only render three child nodes', () => {
       expect(wrapper.children()).toHaveLength(3);
     });
 
     it('should render a HeaderContainer', () => {
-      expect(wrapper.find(HeaderContainer)).toHaveLength(1);
+      expect(wrapper.find(ConnectedHeaderContainer)).toHaveLength(1);
     });
 
     it('should render a PipelineContainer', () => {
@@ -41,13 +44,17 @@ describe('top-level components', () => {
 
   describe('HeaderContainer', () => {
 
-    let wrapper = enzyme.shallow(<HeaderContainer firstName={'Schno'}/>)
+    // const mockStore = configureMockStore();
+    // const store = mockStore({});
+    const wrapper = enzyme.shallow(
+        <HeaderContainer firstName={'Schno'}/>
+    );
     it('should only render two child nodes', () => {
       expect(wrapper.children()).toHaveLength(2);
     });
 
     it('should render a HeaderNav as the second child node', () => {
-      expect(wrapper.find('HeaderNav')).toHaveLength(1);
+      expect(wrapper.find(HeaderNav)).toHaveLength(1);
       expect(wrapper.children()[2]).toEqual(wrapper.find('HeaderNav')[0]);
     });
 
